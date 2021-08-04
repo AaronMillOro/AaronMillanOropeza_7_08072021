8f0e4d328a49be28c7c1caebc1d064997cf816af
AaronMillOro <aaron.ibt@gmail.com>
2021-08-05 00:38:07 +0200

    :hammer: :lock: password validator rules and middleware

---
+++ backend/middleware/password-validator.js
@@ -0,0 +1,13 @@
+const passwordRules = require('../models/Password');
+
+module.exports = (req, res, next) => {
+  try {
+    if (!passwordRules.validate(req.body.password)){
+      return res.status(400).json({ error: 'Password should contain: at least 8 characters, upper and lower case, 1 digit and 1 character' });
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  } 
+};
