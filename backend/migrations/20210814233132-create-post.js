7b7e06e4741668e7bac7d8e8039e14969bdc7490
AaronMillOro <aaron.ibt@gmail.com>
2021-08-15 01:42:08 +0200

    :card_file_box: setup of Post model using sequelize-cli

---
+++ backend/migrations/20210814233132-create-post.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      likes: {
+        defaultValue: 0,
+        type: Sequelize.INTEGER
+      },
+      usersLike: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.BIGINT,
+        references: {
+          model: 'User',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Posts');
+  }
+};
