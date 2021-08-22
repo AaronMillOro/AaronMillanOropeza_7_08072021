3cba5e5f0ef34b3aca1c0474dfed02e02962d312
AaronMillOro <aaron.ibt@gmail.com>
2021-08-22 23:30:15 +0200

    :sparkles: creation of the component Header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<header class="navbar navbar-dark">
+  <div class="navbar-brand">
+    <img src="../assets/icon-left-font-monochrome-white.svg" width="250" height="100" class="d-inline-block align-top" alt="Groupomania logo">
+  </div>
+  <nav class="my-2 my-0 mr3">
+    <NavButton to="/account/" name="Mon compte"/>
+    <NavButton to="/" name="Actualités"/>
+    <NavButton to="/login/" name="Déconnexion"/>
+  </nav>
+</header>
+</template>
+
+<script>
+import NavButton from '@/components/NavButton.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    NavButton,
+  },
+}
+</script>
+
+<style scoped>
+.navbar {
+  background-color: #243447;
+}
+</style>
