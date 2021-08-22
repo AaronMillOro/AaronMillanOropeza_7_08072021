89161c0fc3e8e3e62914a969d3b7fe71bbcf10ab
AaronMillOro <aaron.ibt@gmail.com>
2021-08-22 23:31:55 +0200

    :sparkles: footer component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="bg-secondary text-center pt-5">
+    <p class="text-white py-2">Mentions légales © 2021</p>
+    <p class="bg-dark text-white py-4">Intranet Groupomania</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
