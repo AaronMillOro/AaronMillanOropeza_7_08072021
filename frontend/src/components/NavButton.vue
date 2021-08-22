26cf0bcad079f3e6b5855e0fa71f058725c8c04a
AaronMillOro <aaron.ibt@gmail.com>
2021-08-22 23:28:02 +0200

    :sparkles: navigation button component

---
+++ frontend/src/components/NavButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link to={url} class="btn btn-outline-light px-2 mx-2">{{ name }}</router-link>
+</template>
+
+<script>
+export default {
+  name: "NavButton",
+  props:  {
+    url: String,
+    name: String
+  }
+}
+</script>
