35ed2e67f6f51fa8897cf1c5f4324ba8ebd50417
AaronMillOro <aaron.ibt@gmail.com>
2021-08-22 23:26:41 +0200

    :sparkles: creation of title component

---
+++ frontend/src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container py-2">
+  <div class="row justify-content-center"> 
+    <h1 class="text-info py-2">{{ titleName }}</h1>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    titleName: String
+  }
+}
+</script>
