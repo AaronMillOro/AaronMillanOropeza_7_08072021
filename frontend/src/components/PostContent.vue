b735fe4bda85fbbcd346eae1dea63e87728ecb7e
AaronMillOro <aaron.ibt@gmail.com>
2021-08-28 04:30:09 +0200

    :sparkles: Post view has layout with post information
    Formated with bootstrap style, minor changes compared to the prototype

--- frontend/src/components/PostContent.vue
+++ frontend/src/components/PostContent.vue
@@ -1,8 +1,40 @@
 <template>
   <div>
-    {{ post }} 
-    {{ user }} 
-    {{ opinions }} 
+    <div class="card border-info my-4">
+      <div class="card-header border-info bg-dark">
+        <div class="d-flex flex-row align-items-center">
+          <div class="mr-2 col-sm-3">
+            <img :src="this.user.imageUrl" :alt="'user_post_' + this.post.id" class="rounded-circle" width="80" v-if="this.user.imageUrl !== null">
+            <img src="/img/icon-left-font-monochrome-white.4891a9da.svg" :alt="'user_post_' + this.post.id" class="rounded-circle" width="80" v-else>
+          </div>
+          <div class="ml-2 col-sm-8">
+            <div class="font-weight-bold m-0 text-white">{{ this.user.pseudo }}</div>
+            <p class="card-text justify-content-center">
+              <small class="text-white"> {{ this.date }} à {{ this.time }} </small>
+            </p>
+          </div>
+          <div class="mx-2 col-sm-1"> 
+            <BIconTrashFill class="text-white" v-if="this.dataPost.canDelete === true"/> 
+            <BIconTrashFill class="text-white" v-if="this.dataPost.canDelete === 'all'"/> 
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <p class="card-text text-center text-dark">{{ this.post.text }}</p>
+        <div class="text-center"> <img v-show="this.post.imageUrl !== null" class="img-fluid" :src="this.post.imageUrl" height="200" :alt="'image_pub_'+ this.post.id"> </div>
+      </div>
+        <div class="card-footer border-info bg-white">
+          <div class="d-flex flex-row align-items-center">
+            <div class="mr-2">
+              <p class="card-text mr-0"> 
+                {{ this.post.likes }} 
+                <BIconHeart class="text-danger" role="img" aria-label="Heart" v-if="this.dataPost.heart === 'off_heart'" />
+                <BIconHeartFill class="text-danger" role="img" aria-label="Filled Heart" v-else/>
+              </p></div>
+            <div class="ml-3"> <p class="card-text mr-0"> {{ this.post.countOpinions }} commentaires</p> </div>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -13,9 +45,11 @@ export default {
   name: 'PostContent',
   data() {
     return {
-      post: "",
+      dataPost: "",
       user: "",
-      opinions: [],
+      post: "",
+      date: "",
+      time: ""
     };
   },
   created() {
@@ -26,13 +60,16 @@ export default {
       { headers }
     )
       .then(res => {
+        this.dataPost = res.data;
         this.post = res.data.post;
         this.user = res.data.post.User;
-        this.opinions = res.data.post.opinions;
-        console.log(res);
+        this.date = this.post.createdAt.split('T')[0];
+        this.time = this.post.createdAt.split('T')[1].split('.')[0];
+        console.log(this.$route.params.id_post);
+        return console.log(res);
       })
       .catch(error => {
-        console.log(error)
+        return console.log(error)
       });
   }
 }
